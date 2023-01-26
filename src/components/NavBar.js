ef6dbf59574c9f819a853a6950e08053a3cb0447
Arianna Bunn <ajbunny12@gmail.com>
2023-01-25 23:49:06 -0500

    final bootstrap push

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {useNavigate, Link } from "react-router-dom";
-import  Button  from "react-bootstrap/button"
+import {useNavigate } from "react-router-dom";
+import  Button  from "react-bootstrap/Button"
 
 
 export default function Navi() {
